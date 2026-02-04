cf5c87a29dcbf39592ee967133019730f3d9a3d6
laoseric <laoseric1025@gmail.com>
2026-02-04 17:33:40 -0600

    Update love meter messages in config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "MARRY ME BABYYYY?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Oh dang fr! 🚀💝",              // Shows when they go past 1000%
+        normal: "Type shit fr! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
