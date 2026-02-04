e701328c383444c8f18a90bbf0b0932ebc0f8ba2
laoseric <laoseric1025@gmail.com>
2026-02-04 17:25:57 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dn15o2g95/raw/upload/v1770247372/7403598286709411103_tvnmgd", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dn15o2g95/video/upload/v1770247533/Lady_Gaga_Bruno_Mars_-_Die_With_A_Smile_Official_Music_Video_cr2axn.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
